8e7c1999b58ddd34f34bc11357d7d1a4d0ad2489
luka_pavcnik <lukapavcnik5@gmail.com>
2020-02-11 11:49:30 +0100

    Init Commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="nav">
+        <h1>9Gag</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Navbar',
+    }
+</script>
+
+<style>
+    .nav {
+        top: 0;
+        left: 0;
+        background: black;
+        width: 100vw;
+        margin: 0;
+    }
+</style>
